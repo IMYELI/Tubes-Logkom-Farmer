5285c7ac5ef490f7903654c118b2e1ca76455a21
gibrandarmawan <88297926+gibrandarmawan@users.noreply.github.com>
2021-11-21 19:43:14 +0700

    Menu marketplace based on seasons

---
+++ marketplace.pl
@@ -0,0 +1,49 @@
+marketSpring :-
+    nl,
+    write('=====Marketplace====='), nl,
+    write('What do you want to buy?'), nl,
+    write('1. Corn seed (50 golds)'), nl,
+    write('5. Chicken (500 golds)'), nl,
+    write('6. Sheep (1000 golds)'), nl,
+    write('7. Cow (1500 golds)'), nl,
+    write('8. Level 2 shovel (300 golds)'), nl,
+    write('9. Level 2 Fishing rod (500 golds)'), nl,
+
+marketSummer :-
+    nl,
+    write('=====Marketplace====='), nl,
+    write('What do you want to buy?'), nl,
+    write('1. Tomato seed (50 golds)'), nl,
+    write('2. Chicken (500 golds)'), nl,
+    write('3. Sheep (1000 golds)'), nl,
+    write('4. Cow (1500 golds)'), nl,
+    write('5. Level 2 shovel (300 golds)'), nl,
+    write('6. Level 2 Fishing rod (500 golds)'), nl,
+
+marketFall :-
+    nl,
+    write('=====Marketplace====='), nl,
+    write('What do you want to buy?'), nl,
+    write('1. Potato seed (50 golds)'), nl,
+    write('2. Chicken (500 golds)'), nl,
+    write('3. Sheep (1000 golds)'), nl,
+    write('4. Cow (1500 golds)'), nl,
+    write('5. Level 2 shovel (300 golds)'), nl,
+    write('6. Level 2 Fishing rod (500 golds)'), nl,
+
+marketWinter :-
+    nl,
+    write('=====Marketplace====='), nl,
+    write('What do you want to buy?'), nl,
+    write('1. Carrot seed (50 golds)'), nl,
+    write('2. Chicken (500 golds)'), nl,
+    write('3. Sheep (1000 golds)'), nl,
+    write('4. Cow (1500 golds)'), nl,
+    write('5. Level 2 shovel (300 golds)'), nl,
+    write('6. Level 2 Fishing rod (500 golds)'), nl,
+
+    
+
+
+
+
