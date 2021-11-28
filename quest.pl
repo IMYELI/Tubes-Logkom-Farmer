713f514d5217b7bb900a3bcde94593aba6076278
ghebyon <13520079@std.stei.itb.ac.id>
2021-11-28 10:08:49 +0700

    update telat.... tunggu ya

--- quest.pl
+++ quest.pl
@@ -1,31 +1,98 @@
 % Not Yet a
 :- use_module(library(random)).
-:- dynamic(currentQuest/5).
-:- dynamic(quest_status/1).
-:- dynamic(panen_quest/7).
-:- dynamic(ikan_quest/7).
-:- dynamic(ranching_quest/7).
+:- dynamic(getQuest/0).
+:- dynamic(goalQuest/2).
+
+isQuestActive(false).
+goalQuest(harvest, 0).
+goalQuest(fish, 0).
+goalQuest(ranch, 0).
+getQuestreward(0,0).
 
 
-startQuest:- 
-    \+quest_status(_),
-    write('----------------------------------------------------------\n'),
-    write('|                         QUEST                          |\n'),
-    write('| Kamu akan menerima quest untuk mengumpulkan beberapa   |\n'),
-    write('| harvest item, ikan, dan beberapa ranching item         |\n'),
-    write('| 1. Terima dan lakukan quest                            |\n'),
-    write('| 2. Batalkan                                            |\n'),
-    write('----------------------------------------------------------\n'),
-    write('Masukkan pilihan anda (terima. / batalkan.) : '),
-    read(Response),
-    (Response == terima) -> assert(quest_status(1));
+startQuest :- 
+    write('Hei bro, u want some challange?'),nl,
+    isQuestActive(Check),
+    (Check = true -> 
+        write('Bruh, u have another job. Please be more diciplined. Finish your current job first\n')),
+        exitQuest;
+     Check = false->
+        retract(goalQuest(harvest,_)),
+        retract(goalQuest(fish,_)),
+        retract(goalQuest(ranch,_)),
 
-/*Generate item*/
+        write('----------------------------------------------------------\n'),
+        write('|                         QUEST                          |\n'),
+        write('| You will receive a quest to collect some               |\n'),
+        write('| harvest items, fish, and ranching items                |\n'),
+        write('| 1. Accept challange                                    |\n'),
+        write('| 2. Cancel                                              |\n'),
+        write('----------------------------------------------------------\n'),
+        write('(accept./cancel.) >>> '),
+        read(Response),
+        nl,
+        (Response = accept -> generateQuest;
+         Response = cancel -> exitQuest)
 
-/*Show Quest Status*/
-questStatus:- 
-    \+quest_status(_),
-    write('No active quest right now. Go to Q to receive quest'),nl,!.
+generateQuest :-
+    write('There are 4 difficulty of quests that you can accept.\n'),
+    write('The more difficult the quest, the more rewards you will receive.\n'),
+    write('1. easy\n'),
+    write('2. medium\n'),
+    write('3. hard\n'),
+    write('4. asian\n'),
+    write('Which difficulty u want? '),
+    read(DifficultyQuest),
+    nl,
+    (
+        DifficultyQuest = easy -> generateEzQuest;
+        DifficultyQuest = medium -> generateMedQuest;
+        DifficultyQuest = hard -> generateHardQuest;
+        DifficultyQuest = asian -> generateAsianQuest;
+    ).
+
+generateEzQuest :-
+    retract(isQuestActive(_)),
+    asserta(isQuestActive(yes)),
+    random_between(1,3,rndmEzHarvest),
+    random_between(1,3,rndmEzFish),
+    random_between(1,3,rndmEzRanch),
+    asserta(goalQuest(harvest,rndmEzHarvest)),
+    asserta(goalQuest(fish,rndmEzFish)),
+    asserta(goalQuest(ranch,rndmEzRanch)).
+
+generateMedQuest :-
+    retract(isQuestActive(_)),
+    asserta(isQuestActive(yes)),
+    random_between(3,6,rndmMedHarvest),
+    random_between(3,6,rndmMedFish),
+    random_between(3,6,rndmMedRanch),
+    asserta(goalQuest(harvest,rndmMedHarvest)),
+    asserta(goalQuest(fish,rndmMedFish)),
+    asserta(goalQuest(ranch,rndmMedRanch)).
+
+generateHardQuest :-
+    retract(isQuestActive(_)),
+    asserta(isQuestActive(yes)),
+    random_between(6,11,rndmHardHarvest),
+    random_between(6,11,rndmHardFish),
+    random_between(6,11,rndmHardRanch),
+    asserta(goalQuest(harvest,rndmHardHarvest)),
+    asserta(goalQuest(fish,rndmHardFish)),
+    asserta(goalQuest(ranch,rndmHardRanch)).
+
+generateAsianQuest :-
+    retract(isQuestActive(_)),
+    asserta(isQuestActive(yes)),
+    random_between(11,50,rndmAsianHarvest),
+    random_between(11,50,rndmAsianFish),
+    random_between(11,50,rndmAsianRanch),
+    asserta(goalQuest(harvest,rndmAsianHarvest)),
+    asserta(goalQuest(fish,rndmAsianFish)),
+    asserta(goalQuest(ranch,rndmAsianRanch)).
+
+
+/*
 
 questStatus:- 
     quest_status(_),
@@ -33,3 +100,4 @@ questStatus:-
     panen_quest(_) -> (write('-  harvest item'),nl),
     ikan_quest(_) -> (write('-  fish'),nl),
     ranching_quest(_) -> (write('-  ranching item'),nl).
+*/
